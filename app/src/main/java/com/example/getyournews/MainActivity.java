841bf53d4429a91f609063c1e449724884e3f0a2
Ankit Sharma <sharmankit9593@gmail.com>
2021-08-28 02:26:31 +0530

    fixed errors

--- app/src/main/java/com/example/getyournews/MainActivity.java
+++ app/src/main/java/com/example/getyournews/MainActivity.java
@@ -1,31 +1,70 @@
 package com.example.getyournews;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.getyournews.Models.ApiResponse;
 import com.example.getyournews.Models.NewsHeadlines;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements SelectListener{
+public class MainActivity extends AppCompatActivity implements SelectListener, View.OnClickListener{
     RecyclerView recyclerView;
     CustomAdapter customAdapter;
     ProgressDialog progressDialog;
+    Button b1, b2, b3, b4, b5, b6, b7;
+    SearchView searchView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         progressDialog = new ProgressDialog(this);
         progressDialog.setTitle("Getting news for you...");
         progressDialog.show();
 
+
+
+        b1 = findViewById(R.id.btn_1);
+        b1.setOnClickListener(this);
+        b2 = findViewById(R.id.btn_2);
+        b2.setOnClickListener(this);
+        b3 = findViewById(R.id.btn_3);
+        b3.setOnClickListener(this);
+        b4 = findViewById(R.id.btn_4);
+        b4.setOnClickListener(this);
+        b5 = findViewById(R.id.btn_5);
+        b5.setOnClickListener(this);
+        b6 = findViewById(R.id.btn_6);
+        b6.setOnClickListener(this);
+        b7 = findViewById(R.id.btn_7);
+        b7.setOnClickListener(this);
+        searchView = findViewById(R.id.search_view);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String query) {
+
+                RequestManager manager = new RequestManager(MainActivity.this);
+                manager.getNewsHeadlines(listener, "general", query);
+                return true;
+            }
+        });
         RequestManager manager = new RequestManager(this);
         manager.getNewsHeadlines(listener, "general", null);
     }
@@ -33,13 +72,19 @@ public class MainActivity extends AppCompatActivity implements SelectListener{
     private final OnFetchDataListener<ApiResponse> listener = new OnFetchDataListener<ApiResponse>() {
         @Override
         public void onFetchData(List<NewsHeadlines> list, String message) {
-            showNews(list);
-            progressDialog.dismiss();
+            if(list.isEmpty()) {
+                Toast.makeText(MainActivity.this, "No Data Found", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                showNews(list);
+                progressDialog.dismiss();
+            }
+
         }
 
         @Override
         public void onErrorMessage(String message) {
-
+            Toast.makeText(MainActivity.this, "Uh-oh, its not you! its us! Please try after some time", Toast.LENGTH_SHORT).show();
         }
     };
 
@@ -57,5 +102,15 @@ public class MainActivity extends AppCompatActivity implements SelectListener{
         startActivity(new Intent(MainActivity.this, DetailsActivity.class)
         .putExtra("data", headlines));
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        Button button = (Button) view;
+        String category = button.getText().toString();
+        progressDialog.setTitle("Fetching " + category + " articles...");
+        progressDialog.show();
+        RequestManager manager = new RequestManager(this);
+        manager.getNewsHeadlines(listener, category, null);
     }
 }
