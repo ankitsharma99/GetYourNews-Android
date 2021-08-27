59428c4a00ce7aa19485632644ffe9ff78973f0c
Ankit Sharma <sharmankit9593@gmail.com>
2021-08-28 01:25:08 +0530

    Updated UI

--- app/src/main/java/com/example/getyournews/DetailsActivity.java
+++ app/src/main/java/com/example/getyournews/DetailsActivity.java
@@ -3,6 +3,7 @@ package com.example.getyournews;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -17,6 +18,9 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+//        getSupportActionBar().hide();
 
         tv_title = findViewById(R.id.tv_detailsTitle);
         tv_author = findViewById(R.id.tv_detailAuthor);
